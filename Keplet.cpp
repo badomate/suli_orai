f8e1a40ea27b082a46bd935a3ff4a65435b85212
badomtig <71382874+badomtig@users.noreply.github.com>
2020-09-16 14:18:03 +0200

    Update Keplet.cpp

--- Keplet.cpp
+++ Keplet.cpp
@@ -7,5 +7,5 @@ float keplet(float a, float b, float c, float alfa)
 {
     float radian = (alfa/180)*M_PI;
 
-    return (a+b)/(pow(c,2))*sqrt(3+pow(sin(a), 2)+2*pow(cos(a), 3));
+    return (a+b)/(pow(c,2))*sqrt(3+pow(sin(radian), 2)+2*pow(cos(radian), 3));
 }
