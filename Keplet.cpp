44e64da6cb3894fc7157a883a6db05b0ac21fbb9
badomtig <71382874+badomtig@users.noreply.github.com>
2020-09-16 14:15:54 +0200

    Create Keplet.cpp

---
+++ Keplet.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <windows.h>
+#include <iomanip>
+#include <math.h>
+
+float keplet(float a, float b, float c, float alfa)
+{
+    float radian = (alfa/180)*M_PI;
+
+    return (a+b)/(pow(c,2))*sqrt(3+pow(sin(a), 2)+2*pow(cos(a), 3));
+}
