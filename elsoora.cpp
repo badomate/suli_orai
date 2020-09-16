ed5b1f58cc344cc37982cb7be53984cbc12ee834
badomtig <71382874+badomtig@users.noreply.github.com>
2020-09-16 13:12:26 +0200

    Create elsoora.cpp

---
+++ elsoora.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+
+void elosoora()
+{
+
+    cout << "Elsõ feladat:" << endl << endl;
+    cout << "Ez az elsõ mondat.";
+    cout << "Második mondat.\n";
+    cout << "Harmadik mondat.";
+    cout << "\nNegyedik mondat." << endl << "Itt a vége!";
+
+    Sorhatar();
+
+    cout << "Második feladat:" << endl << endl;
+    cout << "Gyümölcsök\n\n";
+    cout << "\tAlma\t\tApple\n";
+    cout << "\tKörte\t\tPear\n";
+
+    Sorhatar();
+
+    cout << "Harmadik feladat:" << endl << endl;
+    cout << "   *"    << endl;
+    cout << "  / \\"   << endl;
+    cout << " /   \\"  << endl;
+    cout << "/_____\\" << endl;
+    cout << "|     |" << endl;
+    cout << "|     |" << endl;
+    cout << "|     |" << endl;
+    cout << "|     |" << endl;
+    cout << "|_____|" << endl;
+
+    Sorhatar();
+
+    cout << "Ötödik feladat:" << endl << endl;
+    cout << "Én vagyok a híres egyfejû,\nA nevem is ennyi csak: Süsü ...\nÓ, ha rózsabimbó leehtnék....\nRám szállnának a lepkék!";
+
+    Sorhatar();
+
+    cout << "\"\n\t\'\n\\" << endl;
+
+    Sorhatar();
+
+    cout << "Hatodik feladat:" << endl << endl;
+    cout << "33*77=" << 33*77 << endl;
+    cout << "2+2=" << 2+2 <<  "4+4=" << 4+4 <<  "8+8=" << 8+8 << endl;
+
+    Sorhatar();
+
+    cout << "Hetedik feladat:" << endl << endl;
+    int magassag;
+    cout << "Milyen magas vagy: "<< endl;
+    cin >> magassag;
+    cout << "Ilyen magas vagy:" << magassag << "cm" << endl;
+
+    Sorhatar();
+
+    cout << "Nyolcadik feladat:" << endl << endl;
+    string Nev;
+    cout << "Mi a neved: " << endl;
+    cin >> Nev;
+    cout << "Szia" << Nev << endl;
+
+    Sorhatar();
+    int x;
+    cout << "Kilencedik feladat:" << endl << endl;
+    cout << "Adj meg egy szamot" << endl;
+    cin >> x;
+    cout << "A 2.feladat tesztje: " << x << endl;
+
+    Sorhatar();
+
+    cout << "Tizedik feladat:" << endl << endl;
+    string szo1 = "baba";
+    string szo2 = "abba";
+    cin.get();
+
+}
