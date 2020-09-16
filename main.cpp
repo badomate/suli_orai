07906be977af03f80b843eb1795cc4677e7d81e0
badomtig <71382874+badomtig@users.noreply.github.com>
2020-09-16 13:10:30 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+using namespace std;
+{
+  int main()
+  {
+      //elosora();
+      int a;
+      string nev;
+
+      cout << "Hány éves vagy: " << endl;
+      cin >> a;
+
+      cout << "Mi a neved" << endl;
+      cin.sync();
+      getline(cin, nev);
+
+      cout << endl << nev << " " << a;
+
+
+  }
+}
