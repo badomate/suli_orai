f80617ae70bb04d6b75f3cb82f76d2c9c89db1fb
badomtig <71382874+badomtig@users.noreply.github.com>
2020-09-16 13:11:36 +0200

    Create Sorhatar.cpp

---
+++ Sorhatar.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+void Sorhatar()
+{
+    cout << endl;
+    cout << "________________________________________________________________________" << endl;
+    cout << endl;
+}
